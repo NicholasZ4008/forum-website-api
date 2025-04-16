7926832237fb761974b32af4cafbe586db7b049b
Zhang <nicholas.zhang@phsa.ca>
2025-04-16 14:08:45 -0700

    more sanity

--- Program.cs
+++ Program.cs
@@ -17,9 +17,13 @@ builder.Services.AddCors(options =>
                       policy  =>
                       {
                           policy.WithOrigins("http://localhost:3000",
-                                              "https://theforumuniversity.com",
-                                              "https://www.theforumuniversity.com",
-                                              "https://api.theforumuniversity.com"
+                                            "https://localhost:3000",
+                                            "http://theforumuniversity.com",
+                                            "https://theforumuniversity.com",
+                                            "http://www.theforumuniversity.com",
+                                            "https://www.theforumuniversity.com",
+                                            "http://api.theforumuniversity.com",
+                                            "https://api.theforumuniversity.com"
                                               )
                                               .AllowAnyHeader()
                                               .AllowAnyMethod();
